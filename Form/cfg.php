ec0f2f2b2f2f6310a1151c175834597abe41bbc5
dubr <dubr.cola@gmail.com>
2017-03-17 16:23:19 +0300

    enable form action

--- Form/cfg.php
+++ Form/cfg.php
@@ -3,6 +3,7 @@ return array(
     'actions' => array(
         'form' => array(
             'name' => 'Форма',
+            'disabled' => false,
             'install' => function($ib, $ctr, $params) {
                 $ctr->install($ib, $ctr, $params);
             }
